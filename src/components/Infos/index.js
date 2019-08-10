b086fdc2dc4a2f41e97d2e54392cc1ae6a2b6e9e
José Fagundes <fagundesjg@outlook.com>
2019-08-09 23:20:08 -0300

    view users online

--- src/components/Infos/index.js
+++ src/components/Infos/index.js
@@ -1,25 +1,36 @@
-import React from "react";
-
+import React, { Component } from "react";
+import api from "../../services/api";
 import "./styles.scss";
 
-export default function Infos() {
-	return (
-		<div className="info">
-			<p>
-				Login Server:{" "}
-				<font color="FF0000">
-					<b>OFFLINE</b>
-				</font>
-			</p>
-			<p>
-				Game Server:{" "}
-				<font color="FF0000">
-					<b>OFFLINE</b>
-				</font>
-			</p>
-			<p>
-				Players Online: <b>0</b>
-			</p>
-		</div>
-	);
+export default class Infos extends Component {
+	state = {
+		usersOnline: 0
+	};
+
+	componentDidMount = async () => {
+		const users = await api.get("/characters/filter=players_online");
+		this.setState({ usersOnline: users.length });
+	};
+
+	render() {
+		return (
+			<div className="info">
+				<p>
+					Login Server:{" "}
+					<font color="FF0000">
+						<b>OFFLINE</b>
+					</font>
+				</p>
+				<p>
+					Game Server:{" "}
+					<font color="FF0000">
+						<b>OFFLINE</b>
+					</font>
+				</p>
+				<p>
+					Players Online: <b>{this.state.usersOnline}</b>
+				</p>
+			</div>
+		);
+	}
 }
